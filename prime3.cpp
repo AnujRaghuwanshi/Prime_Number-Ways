0fbbcec3ae6263cd293a3721289924d4d7ffdc85
AnujRaghuwanshi <124479521+AnujRaghuwanshi@users.noreply.github.com>
2024-03-10 01:19:16 +0530

    Add files via upload
    We can also determine whether a number is prime or not by running loop from 2 to n/2 where n is number.

---
+++ prime3.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPrime(int n)
+{
+	if (n <= 1)
+		return false;
+
+	for (int i = 2; i <= n / 2; i++)
+		if (n % i == 0)
+			return false;
+
+	return true;
+}
+
+int main()
+{
+    int num;
+    cout<<"Enter natural number: ";
+    cin>>num;
+	isPrime(num) ? cout << " Prime\n" : cout << " Not Prime\n";
+	
+	return 0;
+}
+
+/*Run a loop from i=2 to n/2.
+Inside the loop, check whether n is divisible by i.
+If n is not divisible by i, it means n is a prime number.
+If n is divisible by i, it means n has divisors other than 1 and itself, and thus, it is not a prime number.
+*/
